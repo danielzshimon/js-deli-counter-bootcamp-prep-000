83c5e26fc5c949ca4400d22431be44b8baf0ed40
Daniel Shimon <danielzshimon@gmail.com>
2018-03-08 17:11:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var katzDeli  = [];
 
-function takeANumber(katzDeliLine, name){
+function takeANumber(katzDeli, name){
   katzDeliLine.push(name);
     return ("Welcome, "  + name +  ". You are number " + (katzDeliLine.length)+  " in line.");
 }
